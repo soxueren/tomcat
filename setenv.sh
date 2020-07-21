05ca746b5b057ce06f3e78c5a830d87cbda0b511
soxueren <250029975@qq.com>
2020-07-21 14:56:01 +0800

    Update setenv.sh
    1

--- setenv.sh
+++ setenv.sh
@@ -1,1 +1,1 @@
-export JAVA_OPTS="-Dswitch.env=dev -Djava.library.path=/usr/local/tomcat/native-jni-lib -Dfile.encoding=UTF-8 -server -Xms2048m -Xmx2048m -XX:MaxTenuringThreshold=10 -XX:NewRatio=2 -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC"
+export JAVA_OPTS="-Dswitch.env=dev -Dfile.encoding=UTF-8 -server -Xms2048m -Xmx2048m -XX:MaxTenuringThreshold=10 -XX:NewRatio=2 -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC"
